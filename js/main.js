83b57558b5426a77aab4fc2bc6d658b898175943
Franklin <71532004+fran1014@users.noreply.github.com>
2023-05-27 17:51:34 +0200

    Complete the list of cities

--- js/main.js
+++ js/main.js
@@ -29,9 +29,56 @@ printCities(ciudades, ulCiudades);
 select.addEventListener('change', selectOption);
 
 function selectOption(event) {
+  let resultados = [];
+  switch (event.target.value) {
+    case '1':
 
+      resultados = filterByName(ciudades, false)
+      break;
+
+    case '2':
+      resultados = filterByName(ciudades, true)
+      break;
+
+    case '3':
+      resultados = filterbyNum(ciudades, true)
+
+      break;
+
+    case '4':
+      resultados = filterbyNum(ciudades, false)
+
+      break;
+
+    default:
+      resultados = ciudades;
+      break;
+  }
+  printCities(resultados, ulCiudades)
 }
 
+function filterByName(pLista, pBoolean) {
 
+  if (pBoolean) {
+    return [...pLista].sort((actual, siguiente) => {
+      return actual.nombre < siguiente.nombre ? 1 : -1
+    })
+  } else {
+    return [...pLista].sort((actual, siguiente) => {
+      return actual.nombre > siguiente.nombre ? 1 : -1
+    })
+  }
+}
 
+function filterbyNum(pList, pBoolean) {
+  if (pBoolean) {
+    return [...pList].sort((actual, siguiente) => {
+      return siguiente.habitantes - actual.habitantes
 
+    })
+  } else {
+    return [...pList].sort((actual, siguiente) => {
+      return actual.habitantes - siguiente.habitantes
+    })
+  }
+}
